7bc76ae3da874ab33758700df690fca7de643ae8
RiazK <RiazCKhan@gmail.com>
2021-10-08 14:43:32 -0400

    Update: syntax comas

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,11 +1,11 @@
 INSERT INTO users VALUES
 (1, 'Max Payne', 'maxp@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
 (2, 'James Bond', 'james007p@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-(3, 'Sean Combs', 'jayzp@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.').
+(3, 'Sean Combs', 'jayzp@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
 (4, 'Lara Croft', 'tombraiderp@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
 (5, 'Princess Peach', 'link@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
 (6, 'Wonder Woman', 'ww@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-(7, 'Bruce Wayne', 'batman@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
+(7, 'Bruce Wayne', 'batman@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
 (8, 'Sonya Blade', 'mk@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 
