1f8d4a82d3dc868eb7ac096768755683b25cd497
RiazK <RiazCKhan@gmail.com>
2021-10-09 11:14:11 -0400

    Add: all my reservations query

---
+++ 1_queries/5_all_my_reservations.sql
@@ -0,0 +1,24 @@
+SELECT properties.id,
+       properties.title
+       cost_per_night,
+       reservations.start_date,
+       AVG(end_date - start_date) AS average_rating
+FROM properties
+JOIN reservations ON properties.id = property_id
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT 10;
+
+/* Compass Answer
+
+SELECT properties.*, reservations.*, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+AND reservations.end_date < now()::date
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT 10;
+
+*/
