ee1a1f56467c48b22d3196abf274eaca98e9b1f2
RiazK <RiazCKhan@gmail.com>
2021-10-15 10:56:30 -0400

    Update: getAllReservations FN -> getFulfilledReservations FN

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -78,7 +78,7 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function (guest_id, limit = 10) {
+const getFulfilledReservations = function (guest_id, limit = 10) {
   const queryString = `SELECT properties.*, reservations.*, avg(rating) as average_rating 
   FROM reservations JOIN properties ON reservations.property_id = properties.id JOIN property_reviews ON properties.id = property_reviews.property_id
   WHERE reservations.guest_id = $1 AND reservations.end_date < now()::date GROUP BY properties.id, reservations.id ORDER BY reservations.start_date LIMIT $2;`
@@ -94,7 +94,7 @@ const getAllReservations = function (guest_id, limit = 10) {
     })
   return getReservations;
 }
-exports.getAllReservations = getAllReservations;
+exports.getFulfilledReservations = getFulfilledReservations;
 
 
 /// ------ Properties
