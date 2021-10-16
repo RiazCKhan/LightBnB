aedf1d027a664f2a54f8d1704b03154765bbea06
RiazK <RiazCKhan@gmail.com>
2021-10-16 14:50:59 -0400

    Update: reservation POST route

--- LightBnB_WebApp-master/server/apiRoutes.js
+++ LightBnB_WebApp-master/server/apiRoutes.js
@@ -37,6 +37,16 @@ module.exports = function(router, database) {
     })
   })
 
+  router.get('/reservations/:reservation_id', (req, res) => {
+    const reservationId = req.params.reservation_id;
+    database.getIndividualReservation(reservationId)
+    .then(reservation => res.send(reservation))
+    .catch(e => {
+      console.error(e);
+      res.send(e);
+    })
+  })
+
   router.post('/properties', (req, res) => {
     const userId = req.session.userId;
     database.addProperty({...req.body, owner_id: userId})
@@ -49,29 +59,14 @@ module.exports = function(router, database) {
       });
   });
 
-  router.post('/reservations', (req, res) => {
-    const userId = req.session.userId;
-    if (userId) {
-      database.addReservation({...req.body, guest_id: userId})
-      .then(reservation => {
-        res.send(reservation)
-      })
-      .catch(e => {
-        console.error(e);
-        res.send(e);
-      })
-    } 
-  })
-
-  router.get('/reservations/:reservation_id', (req, res) => {
-    const reservationId = req.params.reservation_id;
-    database.getIndividualReservation(reservationId)
-    .then(reservation => res.send(reservation))
-    .catch(e => {
-      console.error(e);
-      res.send(e);
+  router.post('/reservations/:reservationId', (req, res) => {
+    const reservationId = req.params.reservationId;
+    database.updateReservation({...req.body, reservation_id: reservationId})
+    .then(reservation => {
+      res.send(reservation)
     })
   })
+
 
   return router;
 }
