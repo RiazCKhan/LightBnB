aee9f19111d115b7646e5980e399e50bfcc5b754
RiazK <RiazCKhan@gmail.com>
2021-10-17 16:05:16 -0400

    Add: brosw review button behaviour

--- LightBnB_WebApp-master/public/javascript/index.js
+++ LightBnB_WebApp-master/public/javascript/index.js
@@ -6,5 +6,11 @@ $(() => {
       const idData = $(this).attr('id').substring(17);
       views_manager.show('newReservation', idData);
     })
+    $('.review_details').on('click', function() {
+      const idData = $(this).attr('id').substring(15);
+      console.log('index.js idData', idData);
+      getReviewsByProperty(idData).then((data) => {
+        console.log('index.js data promise', data)});
+    })
   });
 });
