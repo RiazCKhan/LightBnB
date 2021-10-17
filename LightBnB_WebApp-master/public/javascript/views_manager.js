27a36ab7515d761857740c57c3589dc18d6aefc7
RiazK <RiazCKhan@gmail.com>
2021-10-17 14:15:10 -0400

    BugFix: stacking reservation details, Add: detach

--- LightBnB_WebApp-master/public/javascript/views_manager.js
+++ LightBnB_WebApp-master/public/javascript/views_manager.js
@@ -12,6 +12,7 @@ $(() => {
     $signUpForm.detach();
     $newReservationForm.detach();
     $updateReservationForm.detach();
+    $('#reservation-details').detach();
 
     let dataTag = "";
 
@@ -48,7 +49,7 @@ $(() => {
   `
         // if there's an error message we want to display that as well:
         const errorMessage = data.error_message ? `<h4>${data.error_message}</h4>` : ``;
-        $(reservationDetails).appendTo($main).delay(2000).slideUp('slow');
+        $(reservationDetails).appendTo($main) // .delay(2000).slideUp('slow');
         $updateReservationForm.appendTo($main);
         $(dataTag).appendTo("#datatag");
         $(errorMessage).appendTo('#error-message');
