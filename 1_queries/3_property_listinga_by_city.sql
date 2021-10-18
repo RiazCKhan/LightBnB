41b0cb6a0a60bf691eb1011b5871c8cb75a24639
RiazK <RiazCKhan@gmail.com>
2021-10-17 20:43:26 -0400

    Update: modify query

--- 1_queries/3_property_listinga_by_city.sql
+++ 1_queries/3_property_listinga_by_city.sql
@@ -1,22 +1,26 @@
-SELECT properties.id, title, cost_per_night, AVG(property_reviews.rating) as average_rating
+/* Compass Answer  */
+
+SELECT properties.*, avg(property_reviews.rating) as average_rating
+FROM properties
+JOIN property_reviews ON properties.id = property_id
+WHERE city LIKE '%ancouv%'
+AND cost_per_night > 100
+GROUP BY properties.id
+HAVING avg(property_reviews.rating) >= 4
+ORDER BY cost_per_night
+LIMIT 10; 
+
+/*
+ My original answer
+ SELECT properties.id, title, cost_per_night, AVG(property_reviews.rating) as average_rating
 FROM properties
 JOIN property_reviews ON properties.id = property_id
 WHERE city LIKE '%ancouv%'
 GROUP BY properties.id
 HAVING AVG(property_reviews.rating) >= 4
 ORDER BY cost_per_night
-LIMIT 10;
-
-/* Compass Answer 
-
-SELECT properties.*, avg(property_reviews.rating) as average_rating
-FROM properties
-JOIN property_reviews ON properties.id = property_id
-WHERE city LIKE '%ancouv%'
-GROUP BY properties.id
-HAVING avg(property_reviews.rating) >= 4
-ORDER BY cost_per_night
 LIMIT 10; 
-
 */
+
+
 
